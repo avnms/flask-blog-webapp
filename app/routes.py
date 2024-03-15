006b062ccd0c3cbfec3dc52344027c9f7b99c7fa
avnms <sarmaakondivnm@icloud.com>
2024-03-15 14:31:53 +1100

    Returning HTML page from view function

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,20 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    return "I love Python!!!"
+    user = {"username": "Sarma"}
+    return (
+        """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home Page - Flask Blog</title>
+    </head>
+    <body>
+        <h1>Hello, """
+        + user["username"]
+        + """!</h1>
+    </body>
+    </html>
+    """
+    )
