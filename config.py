881ace33ce58edce0f0f841c6e717d94e4d6bc27
avnms <sarmaakondivnm@icloud.com>
2024-03-16 22:48:41 +1100

    Updated sqlalchemy database uri in config.py

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
 
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URI"
+    ) or "sqlite:///" + os.path.join(base_dir, "app.db")
