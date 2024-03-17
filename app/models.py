c5e30421cb4f44fd305804a782d2ea819c80d330
avnms <sarmaakondivnm@icloud.com>
2024-03-17 14:32:11 +1100

    Implementation of login user loader decorator

--- app/models.py
+++ app/models.py
@@ -4,7 +4,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
 
 
 class User(UserMixin, db.Model):
