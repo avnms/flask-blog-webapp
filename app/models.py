5485125b28a004e4d9fa2bd40c662e25581768e5
avnms <sarmaakondivnm@icloud.com>
2024-03-17 14:27:07 +1100

    Import and user the UserMixin class from flask_login

--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from datetime import datetime, timezone
 from typing import Optional
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
